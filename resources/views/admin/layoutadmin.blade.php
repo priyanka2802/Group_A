d9174c69d52e80f81890e422b8a565cb15e500f7
Rishabh Verma <Rishabh4402@gmail.com>
2018-04-01 19:34:26 +0530

    datepick

--- resources/views/admin/layoutadmin.blade.php
+++ resources/views/admin/layoutadmin.blade.php
@@ -12,7 +12,7 @@
 
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
-
+    @yield('head')
     <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
